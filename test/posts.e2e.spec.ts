0fec5b9e6329267370f8a5ca6f3e6ba5ca906e6e
미플 <devmeeple@gmail.com>
2024-06-08 18:46:47 +0900

    refactor: posts e2e 그룹화(describe)

--- test/posts.e2e.spec.ts
+++ test/posts.e2e.spec.ts
@@ -7,6 +7,7 @@ import { Repository } from 'typeorm';
 import { Post } from '../src/posts/entities/post.entity';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { TestHelper } from './test.helper';
+import { CreateRequest } from '../src/posts/dto/create-request.dto';
 
 describe('PostsController (e2e)', () => {
   let app: INestApplication;
@@ -32,79 +33,80 @@ describe('PostsController (e2e)', () => {
     await postsRepository.clear();
   });
 
-  it('/posts 게시글을 작성한다', async () => {
-    // given
-    const expectedRequest = {
-      title: '제목입니다',
-      content: '내용입니다',
-    };
+  describe('[POST] /posts', () => {
+    it('게시글을 작성한다', async () => {
+      // given
+      const createRequest = CreateRequest.of('제목입니다', '내용입니다');
 
-    // when
-    const response = await request(app.getHttpServer())
-      .post('/posts')
-      .send(expectedRequest);
+      // when
+      const response = await request(app.getHttpServer())
+        .post('/posts')
+        .send(createRequest);
 
-    // then
-    expect(response.status).toBe(HttpStatus.CREATED);
+      const posts = await postsRepository.find();
 
-    // 데이터베이스에 정상적으로 저장되었는지 확인한다
-    const posts = await postsRepository.find();
+      // then
+      expect(response.status).toBe(HttpStatus.CREATED);
+      expect(posts).toHaveLength(1);
+      expect(posts[0].title).toBe('제목입니다');
+      expect(posts[0].content).toBe('내용입니다');
+    });
 
-    expect(posts).toHaveLength(1);
-    expect(posts[0].title).toBe('제목입니다');
-    expect(posts[0].content).toBe('내용입니다');
+    it('빈 제목이나 내용으로 게시글을 등록할 수 없다', async () => {
+      // given
+      const badCreateRequest = CreateRequest.of('', '');
+
+      // when
+      const response = await request(app.getHttpServer())
+        .post('/posts')
+        .send(badCreateRequest);
+
+      const posts = await postsRepository.find();
+
+      // then
+      expect(response.status).toBe(HttpStatus.BAD_REQUEST);
+      expect(response.body.message[0]).toEqual('제목은 필수입니다');
+      expect(response.body.message[1]).toEqual('내용은 필수입니다');
+      expect(posts).toHaveLength(0);
+    });
   });
 
-  it('/posts 빈 제목이나 내용으로 게시글을 등록할 수 없다', async () => {
-    // given
-    const invalidRequest = {
-      title: '',
-      content: '',
-    };
+  describe('[GET] /posts/:id', () => {
+    it('작성한 게시글 1개를 조회한다', async () => {
+      // given
+      const post = Post.of('제목입니다', '내용입니다');
+      const savedPost = await postsRepository.save(post);
 
-    // when
-    const response = await request(app.getHttpServer())
-      .post('/posts')
-      .send(invalidRequest);
+      // when
+      const response = await request(app.getHttpServer()).get(
+        `/posts/${savedPost.id}`,
+      );
 
-    // then
-    expect(response.status).toBe(HttpStatus.BAD_REQUEST);
-    expect(response.body.message[0]).toEqual('제목은 필수입니다');
-    expect(response.body.message[1]).toEqual('내용은 필수입니다');
+      // then
+      expect(response.status).toBe(HttpStatus.OK);
+
+      expect(response.body.title).toBe('제목입니다');
+      expect(response.body.content).toBe('내용입니다');
+    });
+
+    it('없는 게시글을 조회하면 오류가 발생한다', async () => {
+      // given
+      const notFoundId = 999;
+
+      // when
+      const response = await request(app.getHttpServer()).get(
+        `/posts/${notFoundId}`,
+      );
+
+      // then
+      expect(response.status).toBe(HttpStatus.NOT_FOUND);
+      expect(response.body.message).toEqual(
+        `${notFoundId}번 게시글을 찾을 수 없습니다`,
+      );
+    });
   });
 
-  it('/posts/:id 게시글 단건조회', async () => {
-    // given
-    const post = Post.of('제목입니다', '내용입니다');
-    const savedPost = await postsRepository.save(post);
-
-    // when
-    const response = await request(app.getHttpServer()).get(
-      `/posts/${savedPost.id}`,
-    );
-
-    // then
-    expect(response.status).toBe(HttpStatus.OK);
-
-    expect(response.body.title).toBe('제목입니다');
-    expect(response.body.content).toBe('내용입니다');
-  });
-
-  it('/posts/:id 게시글 단건조회 없는 게시글을 조회하면 오류가 발생한다', async () => {
-    // given
-    const badId = 999;
-
-    // when
-    const response = await request(app.getHttpServer()).get(`/posts/${badId}`);
-
-    // then
-    expect(response.status).toBe(HttpStatus.NOT_FOUND);
-    expect(response.body.message).toEqual(
-      `${badId}번 게시글을 찾을 수 없습니다`,
-    );
-  });
-
-  it('/posts 전체 게시글을 조회한다(페이지네이션)', async () => {
+  it('[GET] /posts 전체 게시글을 조회한다(페이지네이션)', async () => {
     // given
     const posts = Array(100)
       .fill(0)
@@ -128,7 +130,7 @@ describe('PostsController (e2e)', () => {
     expect(response.body.data[9].content).toBe('내용입니다 91');
   });
 
-  it('/posts/:id 게시글을 수정한다', async () => {
+  it('[PATCH] /posts/:id 게시글을 수정한다', async () => {
     // given
     const post = Post.of('제목입니다', '내용입니다');
     const savedPost = await postsRepository.save(post);
@@ -144,7 +146,7 @@ describe('PostsController (e2e)', () => {
     expect(response.status).toBe(HttpStatus.OK);
   });
 
-  it('/posts 게시글 1개를 삭제한다', async () => {
+  it('[DELETE] /posts 게시글 1개를 삭제한다', async () => {
     // given
     const post = Post.of('제목입니다', '내용입니다');
     const savedPost = await postsRepository.save(post);
@@ -158,46 +160,5 @@ describe('PostsController (e2e)', () => {
     expect(response.status).toBe(HttpStatus.OK);
 
     expect(await postsRepository.find()).toHaveLength(0);
-  });
-
-  it('/posts/:id  존재하지 않는 게시글을 조회한다', async () => {
-    // given
-    const notFoundId = 999;
-
-    // when
-    const response = await request(app.getHttpServer())
-      .get(`/posts/${notFoundId}`)
-      .expect(HttpStatus.NOT_FOUND);
-
-    expect(response.body.message).toEqual(
-      `${notFoundId}번 게시글을 찾을 수 없습니다`,
-    );
-  });
-
-  it('/posts/:id 존재하지 않는 게시글을 수정한다', async () => {
-    // given
-    const notFoundId = 999;
-
-    // when
-    const response = await request(app.getHttpServer())
-      .patch(`/posts/${notFoundId}`)
-      .expect(HttpStatus.NOT_FOUND);
-
-    // then
-    expect(response.body.message).toEqual(
-      `${notFoundId}번 게시글을 찾을 수 없습니다`,
-    );
-  });
-
-  it('잘못된 ID로 요청을 보내면 400(Bad Request) 에러가 발생한다', () => {
-    // given
-    const badRequestId = 'bad';
-
-    // when
-    request(app.getHttpServer())
-      .get(`/posts/${badRequestId}`)
-      .expect(HttpStatus.BAD_REQUEST);
-
-    // then 에러메시지를 커스텀하고 결과를 검증할 수 있다
   });
 });
